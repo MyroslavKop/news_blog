b2d0903a976fbadf71443fad51a5d3dab089e2ef
MiroslavKop <kopytovmiroslav@gmail.com>
2023-02-28 22:06:06 +0200

    localization bug fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,17 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import translationEN from "../public/locales/en/translation.json";
+import translationUA from "../public/locales/ua/translation.json";
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  ua: {
+    translation: translationUA,
+  },
+};
 
 i18n
   .use(Backend)
@@ -9,10 +20,13 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
+    resources,
     debug: false,
-
     interpolation: {
       escapeValue: false,
+    },
+    backend: {
+      loadPath: "/news_blog/locales/{{lng}}/{{ns}}.json",
     },
   });
 
