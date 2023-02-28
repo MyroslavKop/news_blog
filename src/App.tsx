9593709db7aea4541fda1820c5ccc383435d46c8
MiroslavKop <kopytovmiroslav@gmail.com>
2023-02-28 22:37:04 +0200

    localization bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/news_blog">
       <CssBaseline />
       <Box
         sx={{
--- src/i18n.ts
+++ src/i18n.ts
@@ -2,17 +2,6 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-import translationEN from "../public/locales/en/translation.json";
-import translationUA from "../public/locales/ua/translation.json";
-
-const resources = {
-  en: {
-    translation: translationEN,
-  },
-  ua: {
-    translation: translationUA,
-  },
-};
 
 i18n
   .use(Backend)
@@ -20,7 +9,6 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
-    resources,
     debug: false,
     interpolation: {
       escapeValue: false,
